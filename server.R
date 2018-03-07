b0bb2a081095b58a1668d33cd72730052f97b0c8
wcools <w.cools@hotmail.com>
2018-03-07 22:34:07 +0100

    cleaned up code ui layout set

--- server.R
+++ server.R
@@ -13,56 +13,159 @@ library(dplyr)     # ddply -> data manipulation
 # server file
 shinyServer(function(input,output,session){
 
+	# calculate effect sizes per time point
+	adjustInput <- reactive({
+		dbFree <- input$dbFree
+		sldNtotal <- as.numeric(as.character(input$sldNtotal))
+		sldEff <- as.numeric(as.character(input$sldEff))
+		dbAlpha <- as.numeric(as.character(input$dbAlpha))
+		dbBeta <- as.numeric(as.character(input$dbBeta))
+		sldBvar <-  as.numeric(as.character(input$sldBvar))
+		sldWvar <-  as.numeric(as.character(input$sldWvar))
+		sldNg1 <- as.numeric(as.character(input$sldNg1))
+		sldNg2 <- as.numeric(as.character(input$sldNg2))
+		sldNg3 <- as.numeric(as.character(input$sldNg3))
+		sldAVg1 <- as.numeric(as.character(input$sldAVg1))
+		sldAVg2 <- as.numeric(as.character(input$sldAVg2))
+		sldAVg3 <- as.numeric(as.character(input$sldAVg3))
+		sldSDg1 <- as.numeric(as.character(input$sldSDg1))
+		sldSDg2 <- as.numeric(as.character(input$sldSDg2))
+		sldSDg3 <- as.numeric(as.character(input$sldSDg3))
 
-# function to get power, given type I error, number of observations, and predictors and eta-squared as effect size
-fpi<-function(eta2,n,alpha,a){
-  f.crit=qf(1-alpha,a-1,n-a)
-  lambda=eta2/(1-eta2)*n
-  fpi = 1-pf(f.crit,a-1,n-a,lambda)
-  return(fpi)}
+		list(sldNtotal=sldNtotal,sldEff=sldEff,dbAlpha=dbAlpha,dbBeta=dbBeta,sldBvar,sldWvar,
+		sldNg1=sldNg1,sldNg2=sldNg2,sldNg3=sldNg3,sldAVg1=sldAVg1,sldAVg2=sldAVg2,sldAVg3=sldAVg3,sldSDg1=sldSDg1,sldSDg2=sldSDg2,sldSDg3=sldSDg3)
+	})
 
-getPlot1 <- function(){
+	# free parameter
+	output$dd.free <- renderUI({
+		selectInput("dbFree", "free parameter:", c("sample size","effect size","type I error","type II error"),selected="sample size")
+	})
+	# total sample size
+	output$sld.ntotal <- renderUI({
+		inx <- adjustInput()
+		xNtotal <- ifelse(inx$sldNtotal<0,128,inx$sldNtotal)
+		sliderInput("sldNtotal", "sample size:", min = -1, max = 256, value = xNtotal)
+	})
+	# effect size f
+	output$sld.eff <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldEff", "effect size:", min = 0, max = 16, value = .25,step=.001)
+	})
+	# type I error, alpha
+	output$db.alpha <- renderUI({
+		selectInput("dbAlpha", "type I error:", c(".001" = ".001",".01" = ".01",".05" = ".05",".1" = ".1"))
+	})
+	# type II error, beta
+	output$db.beta <- renderUI({
+		selectInput("dbBeta", "type II error:", c(".2" = ".2",".15" = ".15",".1" = ".1",".05" = ".05"))
+	})
+	# effect size variance, between
+	output$sld.bvar <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldBvar", "between variance:", min = 0, max = 32, value = 1)
+	})
+	# effect size variance, within
+	output$sld.wvar <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldWvar", "within variance:", min = 0, max = 32, value = 6)
+	})
+	# output ncp critical F
+	output$txt.out <- renderText({
+		inx <- adjustInput()
+		paste0("noncentrality parameter",inx$sldNtotal)
+	})
+	# sample size groups
+	output$sld.ngs <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldNgs", "all groups:", min = 0, max = 100, value = 32)
+	})
+	# sample size group 1
+	output$sld.ng1 <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldNg1", "group 1:", min = 0, max = 100, value = 32)
+	})
+	# sample size group 2
+	output$sld.ng2 <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldNg2", "group 2:", min = 0, max = 100, value = 32)
+	})
+	# sample size group 3
+	output$sld.ng3 <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldNg3", "group 3:", min = 0, max = 100, value = 32)
+	})
+	# sample size group 1
+	output$sld.avg1 <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldAVg1", "group 1:", min = -100, max = 100, value = 0)
+	})
+	# sample size group 2
+	output$sld.avg2 <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldAVg2", "group 2:", min = -100, max = 100, value = 2)
+	})
+	# sample size group 3
+	output$sld.avg3 <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldAVg3", "group 3:", min = -100, max = 100, value = -2)
+	})
+	# sample size group 1
+	output$sld.sdg1 <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldSDg1", "group 1:", min = 0, max = 100, value = 4)
+	})
+	# sample size group 2
+	output$sld.sdg2 <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldSDg2", "group 2:", min = 0, max = 100, value = 4)
+	})
+	# sample size group 3
+	output$sld.sdg3 <- renderUI({
+		inx <- adjustInput()
+		sliderInput("sldSDg3", "group 3:", min = 0, max = 100, value = 4)
+	})
+
+	# function to get power, given type I error, number of observations, and predictors and eta-squared as effect size
+	fpi<-function(eta2,n,alpha,a){
+	  f.crit=qf(1-alpha,a-1,n-a)
+	  lambda=eta2/(1-eta2)*n
+	  fpi = 1-pf(f.crit,a-1,n-a,lambda)
+	  return(fpi)}
+
+	getPowerCurve <- function(){
+		inx <- adjustInput()
 		cexlab=1.25 
 		cexaxis=1.25 
 		par(mgp=c(2.3,1,0)) 
 		eta2seq=seq(0,.3,.001) 
 		alpha=.05 
-		pp=fpi(eta2=eta2seq,n=as.numeric(as.character(input$sst)),alpha=as.numeric(as.character(input$e1)),a=2) 
-		axis(1,pos=0,cex.lab=cexlab,cex.axis=cexaxis) 
-		axis(2,pos=0,at=c(0,input$e1,.2,.4,.6,.8,1),labels=c(0,input$e1,.2,.4,.6,.8,1),cex.lab=cexlab,cex.axis=cexaxis) 
-		abline(h=0.05,col="gray80",lwd=2) 
+		pp=fpi(eta2=eta2seq,n=inx$sldNtotal,alpha=inx$dbAlpha,a=2) 
 		plot(eta2seq,pp,type="l",xlab=expression(eta^2),ylab=expression(pi),ylim=c(0,1),yaxt="n",axes=FALSE, cex.lab=cexlab,cex.axis=cexaxis,lwd=3) 
-}
+		axis(1,pos=0,cex.lab=cexlab,cex.axis=cexaxis) 
+		axis(2,pos=0,at=c(0,inx$dbAlpha,.2,.4,.6,.8,1),labels=c(0,inx$dbBeta,.2,.4,.6,.8,1),cex.lab=cexlab,cex.axis=cexaxis) 
+		abline(h=0.05,col="gray80",lwd=2) 
+	}
 
-# plot power for range of eta_squared values
-
-	output$outpars <- renderPrint({
-	out <- paste0("test\nagain")
-	cat(out)
-	})
-	output$plot2 <- renderPlot({
-		plotOutput(getPlot1(), height = 300, width = 450)
-	}) 
-	output$plot1 <- renderPlot({
-		# F-null and alternative distributions on top of each other with rejection areas
-		createPlot <- function(){
+	# F-null and alternative distributions on top of each other with rejection areas
+	createPlot <- function(){
+		inx <- adjustInput()
 		xmax=15 
 		xx=seq(0,xmax,.001) 
-		fxx=df(xx,input$sst - 3 - 1, 3 - 1) 
+		fxx=df(xx,as.numeric(as.character(inx$sldNtotal)) - 3 - 1, 3 - 1) 
 		mnx=max(fxx)+.2
 		par(mar=c(2.5,0,0,0))
 		outpu2 <- plot(xx,fxx,type="n",xlab="",ylab="",xlim=c(0,xmax),ylim=c(0,2*mnx),xaxt="n",yaxt="n",axes=FALSE)
 		lines(xx,fxx,lwd=2) 
-		cc=qf(1-input$e1,input$sst - 3 - 1, 3 - 1) 
+		cc=qf(1-as.numeric(as.character(inx$dbAlpha)),as.numeric(as.character(inx$sldNtotal)) - 3 - 1, 3 - 1) 
 		text((cc+1.5),.2,"reject H0",col="red")  
 		text(1.3,.2,"do not reject H0") 
 		xxsub=xx[xx>cc] 
 		fxxsub=fxx[xx>cc]   
-		polygon(x=c(cc,xxsub,rev(xxsub),cc),y=c(df(cc,input$sst - 3 - 1, 3 - 1),fxxsub,0*fxxsub,0),col="red") 
+		polygon(x=c(cc,xxsub,rev(xxsub),cc),y=c(df(cc,as.numeric(as.character(inx$sldNtotal)) - 3 - 1, 3 - 1),fxxsub,0*fxxsub,0),col="red") 
 		segments(cc,0,cc,.15) 
 		segments(0,0,0,1.1) 
 		segments(0,0,xmax,0)
-		fxx2=df(xx,input$sst - 3 - 1, 3 - 1,ncp=10) 
+		fxx2=df(xx,as.numeric(as.character(inx$sldNtotal)) - 3 - 1, 3 - 1,ncp=10) 
 		lines(xx,fxx2+mnx,lwd=2) 
 		segments(0,mnx,0,mnx+1.1) 
 		segments(0,mnx,xmax,mnx) 
@@ -74,7 +177,13 @@ getPlot1 <- function(){
 		text(cc,.6,"decision",cex=1.5,pos=4)
 		text(cc-2.9,.3+mnx,expression(italic(F)[list(2,102,6.7)]),cex=1.3,pos=4) 
 		text(cc-2.9,.8,expression(italic(F)[list(2,102)]),cex=1.3,pos=4) 
-		}
+	}
+
+
+	output$plot2 <- renderPlot({
+		plotOutput(getPowerCurve(), height = 100, width = 450)
+	}) 
+	output$plot1 <- renderPlot({
 		plotOutput(createPlot(), height = 300, width = 450)
 	}) 
 
--- ui.R
+++ ui.R
@@ -18,82 +18,58 @@
 shinyUI(fluidPage(
 
 	titlePanel("Exploring power for one-way ANOVA with F-distribution"),
-
 		fluidRow(
-			column(6,
+			column(2,
 				wellPanel(
-					selectInput("freePar", "free parameter:", c("sample size" = "sst","effect size" = "esf","type I error" = "e1","type II error" = "e2"))
+					uiOutput("dd.free")
+				),
+				wellPanel(
+					uiOutput("sld.bvar"),
+					uiOutput("sld.wvar")
 				)
 			),
-			column(6,
-					verbatimTextOutput('outpars')
+			column(2,
+				wellPanel(
+					uiOutput("sld.ntotal"),
+					uiOutput("sld.eff"),
+					uiOutput("db.alpha"),
+					uiOutput("db.beta")
+				)
+			),
+			column(4,
+				wellPanel(
+					textOutput('txt.out')
+				),
+				plotOutput("plot2")
+			),
+			column(4,
+				wellPanel(
+					textOutput('')
+				),
+				plotOutput("plot1")
 			)
 		),
 		fluidRow(
-			column(2,
-					sliderInput("sst", "sample size:", min = 2, max = 256, value = 128),
-					sliderInput("fs", "effect size:", min = 0, max = 16, value = .25,step=.001)
-			),
-			column(2,
-					selectInput("e1", "type I error:", c(".001" = ".001",".01" = ".01",".05" = ".05",".1" = ".1")),
-					selectInput("e2", "type II error:", c(".2" = ".2",".15" = ".15",".1" = ".1",".05" = ".05"))
-			),
-			column(2,
-					sliderInput("Bvar", "between variance:", min = 0, max = 32, value = 1),
-					sliderInput("Wvar", "within variance:", min = 0, max = 32, value = 6)
-			),
-			column(6,
-				wellPanel(
-					# plotOutput("plot2"),
-					plotOutput("plot2")
-				)
-			)
-			
-			
-		# Bvar/Wvar ~ textfields 0-1
-
-# nrPrdtF ~ dropdown(2,3)	-> nrPrd				|| ncp ~ text || crf ~ text || dfn ~ text || dfd ~ text 
-# sample sizes:
-		),
-		fluidRow(
-				  # tags$head(
-					# tags$style(type="text/css", 
-					  # "label.control-label, .selectize-control.single { 
-						 # display: table-cell; 
-						 # text-align: center; 
-						 # vertical-align: middle; 
-					  # } 
-					  # label.control-label {
-						# padding-right: 10px;
-					  # }
-					  # .form-group { 
-						# display: table-row;
-					  # }
-					  # .selectize-control.single div.item {
-						# padding-right: 15px;
-					  # }")
-				  # ),
-
 			column(2,
 					h4("sample sizes"),
-					sliderInput("ss1", "level 1:", min = 0, max = 100, value = 32),
-					sliderInput("ss2", "level 2:", min = 0, max = 100, value = 32),
-					sliderInput("ss3", "level 3:", min = 0, max = 100, value = 0)
+					uiOutput("sld.ng1"),
+					uiOutput("sld.ng2"),
+					uiOutput("sld.ng3"),
+					uiOutput("sld.ngs")
 			),
 			column(2,
-					h4("average"),
-					sliderInput("av1", "level 1:", min = -100, max = 100, value = -2),
-					sliderInput("av2", "level 2:", min = -100, max = 100, value = 0),
-					sliderInput("av3", "level 3:", min = -100, max = 100, value = 2)
+					h4("averages"),
+					uiOutput("sld.avg1"),
+					uiOutput("sld.avg2"),
+					uiOutput("sld.avg3")
 			),
 			column(2,
-					h4("standard deviation"),
-					sliderInput("sd1", "level 1:", min = 0, max = 100, value = 4),
-					sliderInput("sd2", "level 2:", min = 0, max = 100, value = 4),
-					sliderInput("sd3", "level 3:", min = 0, max = 100, value = 4)
+					h4("standard deviations"),
+					uiOutput("sld.sdg1"),
+					uiOutput("sld.sdg2"),
+					uiOutput("sld.sdg3")
 			),
-
-			column(9,
+			column(6,
 				plotOutput("distPlot")
 			)
 		)
